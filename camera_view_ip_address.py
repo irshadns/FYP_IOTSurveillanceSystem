f88a6e999464b86de812623350bf8185379fa38b
Irshad Nawaz <34713341+irshadns@users.noreply.github.com>
2022-05-15 04:43:22 +0500

    Delete camera_view_ip_address.py

--- camera_view_ip_address.py
+++
@@ -1,9 +0,0 @@
-import socket    
-host_name = socket.gethostname()    
-IPAddress = socket.gethostbyname(host_name)    
-print("Your Computer Name is:" + host_name)    
-# print("Your Computer IP Address is:" + IPAddress) 
-myport = ":5000"
-myIP = str(IPAddress)
-camera_view_url = "http://"+myIP+myport
-print("CAMERA_VIEW_URL:", camera_view_url)
