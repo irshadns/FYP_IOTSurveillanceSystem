86fa952d3adc07e8b144de80b88e229b10344a57
Irshad Nawaz <irshadnaw@gmail.com>
2022-05-17 00:35:40 +0500

    refactor: firebase script

--- python_code/firebase_connection.py
+++ python_code/firebase_connection.py
@@ -4,27 +4,53 @@ from config import CONFIG, FLASK_SERVER_PORT
 from time import sleep
 
 
-def connect_firebase():
-    firebase = pyrebase.initialize_app(CONFIG)
-    database = firebase.database()
-    return database
+class FirebaseHandling:
+    def __init__(self) -> None:
+        firebase = pyrebase.initialize_app(CONFIG)
+        database = firebase.database()
+        super(FirebaseHandling, self).__init__()
+
+    # def connect_firebase():
+    #     firebase = pyrebase.initialize_app(CONFIG)
+    #     database = firebase.database()
+    #     return database
+
+    def send_dynamic_urls_to_firebase(self):
+        prefix = "http://"
+        host_name = socket.gethostname()
+        ip_address = socket.gethostbyname(host_name)
+        base_url = prefix + str(ip_address) + FLASK_SERVER_PORT + "/"
+        siren_on_url = base_url + "siren_on/"
+        siren_off_url = base_url + "siren_off/"
+        data = {
+            "url": base_url,
+            "siren_on_url": siren_on_url,
+            "siren_off_url": siren_off_url,
+        }
+        # database = connect_firebase()
+        # sleep(1.5)
+        # database.child("pi_data").update(data)
+        sleep(1.5)
+        self.database.child("pi_data").update(data)
+
+    def send_initial_data(self):
+        data = {
+            "detection": False,
+            "authenticated": False,
+            "view": False,
+        }
+        # database = connect_firebase()
+        # sleep(1.5)
+        # database.child("pi_data").update(data)
+        self.database.child("pi_data").update(data)
+
+    def update_firebase_notification_variable(self, motion_detected: bool):
+        data = {"detection": motion_detected}
+        # database = connect_firebase()
+        # sleep(1.5)
+        # database.child("pi_data").update(data)
+        self.database.child("pi_data").update(data)
 
 
-def send_dynamic_urls_to_firebase():
-    prefix = "http://"
-    host_name = socket.gethostname()
-    ip_address = socket.gethostbyname(host_name)
-    base_url = prefix + str(ip_address) + FLASK_SERVER_PORT + "/"
-    siren_on_url = base_url + "siren_on/"
-    siren_off_url = base_url + "siren_off/"
-    data = {
-        "url": base_url,
-        "siren_on_url": siren_on_url,
-        "siren_off_url": siren_off_url,
-    }
-    database = connect_firebase()
-    sleep(1.5)
-    database.child("pi_data").update(data)
-
-
-send_dynamic_urls_to_firebase()
+FirebaseHandling.send_dynamic_urls_to_firebase()
+FirebaseHandling.send_initial_data()
