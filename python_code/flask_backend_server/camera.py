5a16b4feec26f0a9d795028de9eded1e99fa9066
Irshad Nawaz <irshadnaw@gmail.com>
2022-06-01 19:28:24 +0500

    revert flipping

--- python_code/flask_backend_server/camera.py
+++ python_code/flask_backend_server/camera.py
@@ -16,7 +16,7 @@ class VideoCamera(object):
 
     def flip_if_needed(self, frame):
         if self.flip:
-            return np.flip(frame, 180)
+            return np.flip(frame, 0)
         return frame
 
     def get_frame(self):
