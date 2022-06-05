8f4f1853c7db85692e9dd132eb412b4517c60f12
Irshad Nawaz <irshadnaw@gmail.com>
2022-06-05 21:55:13 +0500

    refactoring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#Modified by smartbuilds.io
-#Date: 27.09.20
-#Desc: This web application serves a motion JPEG stream
+# Modified by smartbuilds.io
+# Date: 27.09.20
+# Desc: This web application serves a motion JPEG stream
 # main.py
 # import the necessary packages
 import RPi.GPIO as GPIO
@@ -9,32 +9,29 @@ from flask import Flask, render_template, Response
 from camera import VideoCamera
 from firebase_connection import FirebaseHandling
 
-pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
+pi_camera = VideoCamera(flip=False)  # flip pi camera if upside down.
 
 # App Globals (do not edit)
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html') #you can customze index.html here
+    return render_template("index.html")  # you can customze index.html here
+
 
 def gen(camera):
-    #get camera frame
+    # get camera frame
     while True:
         frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
 
-@app.route('/video_feed')
+
+@app.route("/video_feed")
 def video_feed():
-    return Response(gen(pi_camera),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-# Take a photo when pressing camera button
-@app.route('/picture')
-def take_picture():
-    pi_camera.take_picture()
-    return "None"
+    return Response(
+        gen(pi_camera), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
 
 
 @app.route("/siren_on/", methods=["GET"])
@@ -75,7 +72,6 @@ def siren_off():
     return "Turning Siren OFF !"
 
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host="0.0.0.0", debug=False)
